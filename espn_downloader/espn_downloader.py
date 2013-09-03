5a7268b6a9634720889679b2af804a491b7c9a8f
Will Adams <willadams+dev@gmail.com>
2013-09-03 19:14:47 -0400

    v0.2  parse command line options with argparse

--- espn_downloader/espn_downloader.py
+++ espn_downloader/espn_downloader.py
@@ -5,6 +5,7 @@
 #  Distributed under the terms of the Modified BSD License.
 #  The full license is in the file LICENSE, distributed with this software.
 
+import argparse
 from datetime import datetime, timedelta
 from lxml import etree
 import os.path
@@ -14,7 +15,6 @@ import urllib.request
 import subprocess
 import re
 from iz_dvd.user_input import prompt_user_list
-
 
 USER_INFO_FILE = os.path.expanduser('~/.config/iz_espn/userdata.xml')
 ESPN_CONFIG_URL = 'http://espn.go.com/watchespn/player/config'
@@ -29,6 +29,21 @@ NETWORK_IDS = {'n360': 'espn3',
                'buzzerbeater': 'buzzerbeater'}
 DOWNLOAD_DIR = os.path.expanduser('~/Videos/espn')
 
+def get_options():
+    parser = argparse.ArgumentParser(prog='iz_espn')
+    parser.add_argument('-d', '--days', default='3', type=int)
+    parser.add_argument('-b', '--bitrate', choices=['max', 'min', 
+                                                    '400k', '800k', '1200k', 
+                                                    '2200k'],
+                        default='max')
+    parser.add_argument('-ls', '--list-sports', action='store_true')
+    parser.add_argument('-s', '--search', action='append')
+    options = parser.parse_args()
+    return options
+
+OPTIONS = get_options()
+
+#-----------------------------------------------------------------------------
 
 def get_replay_url(days=3, channels=['espn3']):
     channels = ','.join(channels)
@@ -43,6 +58,7 @@ def get_replay_url(days=3, channels=['espn3']):
     return events_url
 
 def get_events(url, sport=None):
+    print(OPTIONS)
     xml = etree.parse(url)
     root = xml.getroot()
     if sport:
@@ -57,10 +73,6 @@ def prompt_events(events):
                                           i['sport'],
                                           '{:%-m/%-d}'.format(i['start_time']),
                                           i['name']) for i in event_infos]
-    #~ choices = ['{} {:^12} {}'.format(NETWORK_IDS[i.find('networkId').text], 
-                                     #~ i.find('sportDisplayValue').text, 
-                                     #~ i.find('name').text) 
-               #~ for i in events]
     response = prompt_user_list(choices)
     chosen = events[response]
     return chosen
@@ -82,7 +94,6 @@ def get_network_info(channel='espn3'):
     channel = network.get('name')
     network_info = {'playerId': playerId, 'cdnName': cdnName, 
                     'channel': channel}
-    #~ return playerId, cdnName, channel
     return network_info
 
 def get_event_info(event, file_ext='mp4'):
@@ -108,7 +119,6 @@ def sanitize_filename(name, space_char='.'):
     filename = name
     filename = re.sub(r' ', space_char, filename).strip(space_char)
     filename = re.sub(r'[:/\<>|*?]', ';', filename).strip(';')
-    #~ filename = re.sub(r'"', "'", filename)
     filename = re.sub('["\']', "", filename)
     return filename
 
@@ -148,11 +158,24 @@ def get_rtmp_info(smil_url, quality=None):
     root = xml.getroot()
     video_streams = root.findall('body/switch/video')
     video_bitrates = [int(i.get('system-bitrate')) for i in video_streams]
-    if quality is None:
-        quality = video_bitrates[-1]
+    if quality in ['max', None]:
+        quality = max(video_bitrates)
+    elif quality == 'min':
+        quality = min(video_bitrates)
     else:
-        quality = video_bitrates[quality]
+        if type(quality) == int:
+            bitrate = quality
+        else:
+            bitrate = int(quality.strip('k')) * 1000
+        diffs = [i - bitrate for i in video_bitrates]
+        abs_diffs = [abs(i) for i in diffs]
+        closest = min(abs_diffs)
+        if closest in diffs:
+            quality = video_bitrates[diffs.index(closest)]
+        else:
+            quality = video_bitrates[abs_diffs.index(closest)]
     stream = video_streams[video_bitrates.index(quality)]
+    print(etree.tostring(stream, pretty_print=True).decode())
     playpath = stream.get('src')
     rtmp_url_base = root.find('head/meta').get('base')
     rtmp_url = '{}/?{}'.format(rtmp_url_base, rtmp_auth)
@@ -163,24 +186,25 @@ def download_stream(rtmp_info, path):
     args = ['rtmpdump', '-r', rtmp_info['rtmp_url'],
             '-y', rtmp_info['playpath'], '-m', '360',
             '-e', '-o', path]
+    print('rtmp cmd: {}'.format(' '.join(args)))
     ret_code = 1
     while ret_code != 0:
         ret_code = subprocess.call(args)
     return path
 
-def get_event(event):
+def get_event(event, quality=None):
     event_info = get_event_info(event)
     download_path = os.path.join(DOWNLOAD_DIR, event_info['filename'])
     auth_url = get_auth_url(event)
     smil_url = get_smil_url(auth_url)
-    rtmp_info = get_rtmp_info(smil_url)
+    rtmp_info = get_rtmp_info(smil_url, quality=quality)
     downloaded = download_stream(rtmp_info, download_path)
     return downloaded
 
 def main():
-    events = get_events(get_replay_url(7), sport='FOOTBALL')
+    events = get_events(get_replay_url(OPTIONS.days))
     chosen = prompt_events(events)
-    downloaded = get_event(chosen)
+    downloaded = get_event(chosen, quality=OPTIONS.bitrate)
     
     return 0
 
